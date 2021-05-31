7db74c7f492ad69e58daf3cc175587087591b5bf
sss <sss@gmail.com>
2021-05-31 16:07:19 +0900

    first commit

--- todo.php
+++ todo.php
@@ -9,6 +9,8 @@ $dbh = new PDO("mysql:host=localhost; dbname=todoList; charset=utf8", $db['user_
 
 
 $showAllTask = "";
+$jobs = ["<a href=./todo.php?not-done>未完了</a>", "<a href=./todo.php?done>完了</a><br />"];
+$list = (isset($_GET['done'])) ? "<h2>完了タスク一覧</h2>" : "<h2>未完了タスク一覧</h2>";
 
 if ($_SESSION['loginStatus'] == true) {
     if (isset($_POST['search']) && $_POST['search'] != "") $_SESSION['search'] = $_POST['search'];
@@ -24,82 +26,72 @@ if ($_SESSION['loginStatus'] == true) {
         $searchSql = "&& contents LIKE '%$_SESSION[search]%'";
         $showAllTask = "<a href=./todo.php>全件表示に戻す</a><br />";
 
-        $dateAsc = "<a href=././todo.php?search=$_SESSION[search]&sort=1>日付昇順</a>";
-        $dateDesc = "<a href=././todo.php?search=$_SESSION[search]&sort=2>日付降順</a><br />";
+        $dateAsc = (isset($_GET['done']))
+            ? "<a href=./todo.php?search=$_SESSION[search]&done&sort=asc>締め切り昇順</a>"
+            : "<a href=./todo.php?search=$_SESSION[search]&sort=asc>締め切り昇順</a>";
+        $dateDesc = (isset($_GET['done']))
+            ? "<a href=./todo.php?search=$_SESSION[search]&done&sort=desc>締め切り降順</a><br />"
+            : "<a href=./todo.php?search=$_SESSION[search]&sort=desc>締め切り降順</a><br />";
     } else {
-        $dateAsc = "<a href=./todo.php?sort=1>日付昇順</a>";
-        $dateDesc = "<a href=./todo.php?sort=2>日付降順</a><br />";
+        $dateAsc = (isset($_GET['done']))
+            ? "<a href=./todo.php?done&sort=asc>締め切り昇順</a>"
+            : "<a href=./todo.php?sort=asc>締め切り昇順</a>";
+        $dateDesc = (isset($_GET['done']))
+            ? "<a href=./todo.php?done&sort=desc>締め切り降順</a><br />"
+            : "<a href=./todo.php?sort=desc>締め切り降順</a><br />";
     }
 
     //sort
     if (isset($_GET['sort']) && isset($_GET['search'])) {
-        if ($_GET['sort'] == 1) {
-            $sortSql = "order by deadline ASC";
-        } elseif ($_GET['sort'] == 2) {
-            $sortSql = "order by deadline DESC";
-        }
+        if ($_GET['sort'] == "asc") $sortSql = "order by deadline ASC";
+        elseif ($_GET['sort'] == "desc") $sortSql = "order by deadline DESC";
     } elseif (isset($_GET['sort'])) {
-        if ($_GET['sort'] == 1) {
-            $sortSql = "order by deadline ASC";
-        } elseif ($_GET['sort'] == 2) {
-            $sortSql = "order by deadline DESC";
-        }
+        if ($_GET['sort'] == "asc") $sortSql = "order by deadline ASC";
+        elseif ($_GET['sort'] == "desc") $sortSql = "order by deadline DESC";
     }
-
-
     $sorts = [$dateAsc, $dateDesc];
 
+    //done or not done
+    $statusSql = "";
+    $statusSql = "status = 0 &&";
+    if (isset($_GET['not-done'])) $statusSql = "status = 0 &&";
+    elseif (isset($_GET['done'])) $statusSql = "status = 1 &&";
+    $searchAction = (isset($_GET['done'])) ? "./todo.php?search=$_SESSION[search]&done" : "./todo.php?search=$_SESSION[search]";
+
+
     //default task list
-    $sql = "select * from tasks where user_id=$_SESSION[userId] $searchSql $sortSql";
+    $sql = "select * from tasks where $statusSql user_id=$_SESSION[userId] $searchSql $sortSql";
     $res = $dbh->query($sql);
     $noTasks = "";
-    $location = "";
+    $complete = "";
     $taskList = [];
+    $job = (isset($_GET['done'])) ? "未完了に戻す" : "完了";
 
     foreach ($res as $key => $task) {
         $check = "";
         $tasks[] = $task['contents'];
-        if ($task['status'] == 1) $check = "checked=checked";
 
-        if (isset($_GET['search'])) $location = "<a href=./todo.php?search=$search&complete=$task[contents]>完了</a><br />";
-        else $location = "<a href=./todo.php?complete=$task[contents]>完了</a><br />";
+        $complete = (isset($_GET['search']))
+            ? "<a href=./todo.php?search=$search&status=$task[status]&contents=$task[contents]>$job</a><br />"
+            : $complete = "<a href=./todo.php?status=$task[status]&contents=$task[contents]>$job</a><br />";
 
-        $taskList[] = "<label><input type=hidden name=checkbox[$key] value=0><input type=checkbox name=checkbox[$key] value=1 $check>$task[contents] $task[deadline] $task[created_at] $task[update_at] $location</lavel>";
+
+        $taskList[] = "<p>$task[contents] $task[deadline] $complete</p>";
     }
     if (count($taskList) == 0) $noTasks = "<p>現在タスクはありません</p>";
 }
 
-//click checkbox
-$searchSql = " && contents LIKE '%$_SESSION[search]%'";
-$sendCheckbox = ($_SERVER['REQUEST_METHOD'] == 'POST');
-if ($sendCheckbox) {
-    $checkboxies = $_POST['checkbox'];
-    var_dump($checkboxies);
-    $sql = (isset($_GET['search'])) ? "select * from tasks where user_id=$_SESSION[userId]$searchSql" : "select * from tasks where user_id=$_SESSION[userId]";
-    $res = $dbh->query($sql);
-
-    // $save = false;
-    // foreach ($res as $key => $task) {
-    //     if ($checkboxies[$key] == $task['status']) continue;
-    //     $statusId = $checkboxies[$key];
-    //     $sql = "update tasks set status = $statusId where user_id = $_SESSION[userId] && contents = '$task[contents]'";
-    //     $dbh->query($sql);
-    //     $save = true;
-    // }
-
-    // save
-    // if ($save) {
-    //     header("Location: $_SERVER[REQUEST_URI]");
-    //     exit;
-    // }
-}
 
 //append tasks
 if (isset($_POST['append'])) {
+    if (!empty($_POST['task']) && !empty($_POST['deadline'])) {
     $_SESSION['task'] = $_POST['task'];
     $timeStanp = date("Y-m-d");
-    $sql = "INSERT INTO tasks(id, user_id, status, contents, deadline, created_at, update_at) VALUES (0, $_SESSION[userId], 0, '$_SESSION[task]', '$timeStanp', '$timeStanp', '$timeStanp')";
+    $sql = "INSERT INTO tasks(id, user_id, status, contents, deadline, created_at, update_at) VALUES (0, $_SESSION[userId], 0, '$_SESSION[task]', '$_POST[deadline]', '$timeStanp', '$timeStanp')";
     $res = $dbh->query($sql);
+    }
+    header("Location: $_SERVER[REQUEST_URI]");
+    exit;
 }
 
 //logout
@@ -107,12 +99,17 @@ $setLogout = isset($_GET['logout']);
 if ($setLogout) $_SESSION['loginStatus'] = false;
 
 
-
 //complete click
-if (isset($_GET['complete'])) {
-    $complete = $_GET['complete'];
-    $sql = "DELETE FROM `tasks` WHERE user_id=$_SESSION[userId] && contents='$complete'";
+$contents = "";
+if (isset($_GET['status'])) {
+    $statusId = $_GET['status'];
+    $statusId = ($statusId == 0) ? 1: 0;
+
+    $contents = $_GET['contents'];
+    $sql = "update tasks set status = $statusId where user_id = $_SESSION[userId] && contents = '$contents'";
     $res = $dbh->query($sql);
+    header("Location: ./todo.php");
+    exit;
 }
 
 
@@ -120,7 +117,7 @@ $logout = '<a href="./?logout">logout</a><br />';
 
 $searchForm = "
     <div>
-        <form action=./todo.php?search=$_SESSION[search] method=POST>
+        <form action=$searchAction method=POST>
         <input type=search name=search placeholder=キーワードを入力>
         <input type=submit name=submit value=検索>
         </form>
@@ -131,13 +128,14 @@ $appendTask = "
         <div>
             <form action=./todo.php?append method=POST>
             <input type=text name=task placeholder=タスクを追加>
+            <input type=text name=deadline placeholder=締め切り日を入力>
             <input type=submit name=append value=追加>
             </form>
         </div>
 ";
 
-$taskForm = ['<div>', "<form action=$_SERVER[REQUEST_URI] method=POST>", implode("\n", $taskList), "<input type=submit name=update value=更新>", "</form>", "</div>"];
-
+$taskForm = ['<div>', "<form action=$_SERVER[REQUEST_URI] method=POST>", implode("\n", $taskList), "</form>", "</div>"];
+$header = "タスク名 | 締め切り";
 
 
 ?>
@@ -158,14 +156,17 @@ $taskForm = ['<div>', "<form action=$_SERVER[REQUEST_URI] method=POST>", implode
     //login
     if ($_SESSION['loginStatus'] == true) {
         echo $logout;
+        echo implode(" | ", $jobs);
         echo $searchForm;
-        echo "<h2>タスク一覧</h2>";
+        echo $list;
         echo implode(" | ", $sorts);
-            echo "$showAllTask";
+        echo "$showAllTask";
     }
 
     echo $noTasks;
-    echo "<a href=./todo.php?new-task>+</a>";
+    if (isset($_GET['done'])) echo "";
+    else echo "<a href=./todo.php?new-task>+</a><br />";
+    echo $header;
     if (isset($_GET['new-task'])) echo $appendTask;
     echo implode("\n", $taskForm);
     ?>
